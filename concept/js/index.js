3628eff58c5ab641f4ef0251c5697fb917e082c0
Martin Smetanka <martin.smetanka@gmail.com>
2021-04-23 04:58:03 +0200

    navrh dizajnu

--- concept/js/index.js
+++ concept/js/index.js
@@ -29,17 +29,22 @@ function store(value,id){
 
     p.setAttribute('id',pId);
     p.setAttribute('onclick',`showQ(${id},this.id)`);
-
+    p.setAttribute('style',`margin:1rem`);
     document.getElementById(id).style.display='none';
     p.innerText=value;
 }
 
-
+function skutocne(id){
+    //TODO
+    // 1.modal
+    //poradovnik(this.id)
+    //opytat sa ci naozja chce vymazat otazku
+}
 
 function createQuestion(typ,number){
 
     $('#modal').modal('toggle');
-
+    document.getElementById('test-footer').hidden=false;
     const qDiv=document.createElement('div');
     const test= document.getElementById('test');
     const question =document.createElement('textarea');
@@ -47,29 +52,55 @@ function createQuestion(typ,number){
     const questionNum=document.createElement('h1');
     const button=document.createElement('button');
     const moznostDiv=document.createElement('div');
+    const rmButton=removeBtn();
+    const header=document.createElement('div');
 
-    moznostDiv.setAttribute('id',`moznostDiv-${number}`)
+    moznostDiv.setAttribute('id',`moznostDiv-${qId}`);
 
-    button.innerText='PRIDAJ MOZNOST';
+    button.setAttribute('id',`adder-${qId}`);
+    button.setAttribute('class','btn btn-dark')
 
-    questionNum.innerText='Otazka';
+    button.innerHTML=`<label for=${button.id} style='vertical-align:top;font-size:x-large'>Pridaj moznost</label>`;
+
+    button.setAttribute('style',`justify-self:end;max-width:12rem;max-height:3rem;box-shadow: rgb(38, 57, 77) 0px 20px 30px -10px;`);
+    button.setAttribute('onmouseover',"this.setAttribute('style',' max-width:12rem;max-height:3rem;')");
+    button.setAttribute('onmouseleave',"this.setAttribute('style','max-width:12rem;max-height:3rem;box-shadow: rgb(38, 57, 77) 0px 20px 30px -10px;')");
+   // button.setAttribute('onclick',`skutocne(this.parentElement.parentElement.remove());`);
+
+    questionNum.innerText=qId.toString();
     questionNum.setAttribute('id',`number-${qId}`);
-    qDiv.appendChild(removeBtn());
-    qDiv.appendChild(questionNum);
+    questionNum.setAttribute('class',`poradovnik`);
+    questionNum.setAttribute('style',`font-size: 2.5rem; font-weight: bold`);
+
+    rmButton.setAttribute('style','max-height:2em;margin:0.5em')
+    rmButton.setAttribute('id',`remover-${qId}`);
+
+    header.appendChild(questionNum);
+    header.appendChild(rmButton);
+    header.setAttribute('class', 'header');
+    header.setAttribute('style', 'display:flex;justify-content:space-between;padding:1rem');
+
+    qDiv.appendChild(header);
     qDiv.appendChild(question);
 
     test.appendChild(qDiv);
 
-    qDiv.setAttribute('id',`question-${number}`)
+    qDiv.setAttribute('id',`question-${qId}`)
+    qDiv.setAttribute('class','card');
+    qDiv.setAttribute('style','padding:3rem;margin-bottom:3rem');
     qDiv.appendChild(pDiv);
 
     pDiv.setAttribute('id',`text-${qId}`);
+    pDiv.setAttribute('style',`margin-bottom:2rem`);
 
+    question.required=true;
     question.setAttribute('class',`question type-${typ}`);
+    question.setAttribute('rows',`5`);
+    question.setAttribute('style',`margin-bottom: 1rem`);
     question.setAttribute('id',qId.toString());
     question.setAttribute('onblur',`store(this.value,${qId})`);
 
-    button.setAttribute('onclick',`addChoice(${number},${typ})`)
+    button.setAttribute('onclick',`addChoice(${qId},${typ})`)
 
 
 
@@ -83,35 +114,50 @@ function createQuestion(typ,number){
 
 }
 function removeBtn(){
-    const button=document.createElement('button');
-    button.setAttribute('onclick',`this.parentElement.remove();`);
-    button.innerText='remove';
+    const button=document.createElement('img');
+    button.setAttribute('src','images/trash.png');
+    button.setAttribute('onmouseover',"this.src='images/thrash-active.png'");
+    button.setAttribute('onmouseout',"this.src='images/trash.png'");
+    button.setAttribute('class',"remover");
+    button.setAttribute('style','max-height:1rem;vertical-align:top;margin-right:0.5em;')
+    button.setAttribute('alt','cancel');
+    button.setAttribute('onclick',`this.parentElement.parentElement.remove();poradovnik(this.id)`);
+
+
     return button;
 }
 function addChoice(id,typ){
     const moznostDiv=document.getElementById(`moznostDiv-${id}`)
-    moznostDiv.setAttribute('class',`${id}-options`)
-    moznostDiv.setAttribute('style',`display:grid`)
+    moznostDiv.setAttribute('class',`${id}-options row `)
+    moznostDiv.setAttribute('style',`display:flex;margin-top:2rem`)
     const moznostInternyDiv=document.createElement('div');
     moznostInternyDiv.setAttribute('style','display:flex');
 
     const x=document.createElement('div');
-    x.setAttribute('class','opt');
+    x.setAttribute('class','opt col');
+    x.setAttribute('style','margin:1rem;');
     x.appendChild(removeBtn());
 
 
     const moznost=document.createElement('input');
-
+    moznost.required=true;
     moznost.setAttribute('type','text');
     moznost.setAttribute('class',`${id}-option`);
+
     x.appendChild(moznost);
     if(typ===3){
+        moznost.setAttribute('style','border:  dashed #e24c4b');
         x.setAttribute('class','opt nespravna');
-        const checkbox=document.createElement('input');
-        checkbox.setAttribute('type','checkbox');
+        const checkbox=document.createElement('img');
+        checkbox.setAttribute('src','images/cancel.png');
+        checkbox.setAttribute('alt','wrong');
+        checkbox.setAttribute('style','max-width:1rem;margin-left:0.5rem;vertical-align: middle;horizontal-align:left')
         checkbox.setAttribute('onclick','addTrue(this)');
         x.appendChild(checkbox);
+
     }
+
+
     moznostInternyDiv.appendChild(x);
     moznostDiv.appendChild(moznostInternyDiv);
 
@@ -120,12 +166,80 @@ function addChoice(id,typ){
 function  addTrue(node){
     if(node.parentElement.classList.contains('nespravna')===true){
         node.parentElement.setAttribute('class','opt spravna');
+        node.parentElement.children[1].setAttribute('style','border: dashed  #cfeb6b')
+        node.setAttribute('src','images/valid.png');
     }
     else if(node.parentElement.classList.contains('spravna')===true){
         node.parentElement.setAttribute('class','opt nespravna');
+        node.parentElement.children[1].setAttribute('style','border: dashed #e24c4b')
+        node.setAttribute('src','images/cancel.png');
     }
 
 }
+
+function changeIDs(oldID,newID){
+
+    //funkcia sposobuje bugy tak ju nepouzivat
+    //jej uloha bola iba urovnat html
+    //na konecny stav odosielaneho testu nema vplyv
+    //disable all removers to prevent missmatch
+    document.getElementById(`question-${oldID}`).setAttribute('id',`question-${newID}`);
+
+
+    document.getElementById(`moznostDiv-${oldID}`).setAttribute('id',`moznostDiv-${newID}`);
+    const typ=document.getElementById(oldID).classList[1].split('type-')[1];
+    document.getElementById(`adder-${oldID}`).setAttribute('id',`adder-${newID}`);
+    document.getElementById(`adder-${newID}`).setAttribute('onclick',`addChoice(${newID},${typ})`);
+
+    document.getElementById(`number-${oldID}`).setAttribute('id',`number-${newID}`);
+
+    document.getElementById(`question-${newID}`).children[0].setAttribute('id',`remover-${newID}`);
+
+
+    document.getElementById(`text-${oldID}`).setAttribute('id',`text-${newID}`);
+    document.getElementById(`adder-${newID}`).children[0].setAttribute('for',`adder-${newID}`);
+
+    document.getElementById(oldID).setAttribute('id',newID.toString());
+    document.getElementById(newID).setAttribute('onblur',`store(this.value,${newID})`);
+    //enable them back
+
+
+}
+
+function poradovnik(id) {
+    const otazky = document.getElementsByClassName('question');
+    const cisla = document.getElementsByClassName('poradovnik');
+    let poradie=1;
+
+    let splitId=id.split('remover-')
+
+    if(otazky.length===0){
+        document.getElementById('test-footer').hidden=true;
+        qId=1;
+    }else{
+        for(let i=Number(splitId[1]);i< otazky.length+1;i++){
+
+           // changeIDs(i+1,i);
+        }
+    }
+    for(let otazka of otazky){
+
+       if(poradie!==otazka.id){
+           cisla[poradie-1].innerText=poradie.toString();
+
+           //qId=poradie+1;
+
+
+
+       }
+
+        poradie=poradie+1;
+    }
+
+
+}
+
+
 
 
 
---
+++ concept/js/send.js
@@ -0,0 +1,48 @@
+
+document.getElementById('send').onclick=function (){
+    let test={};
+    test.nazov=document.getElementById('meno-testu').value;
+    test.casovy_limit=document.getElementById('cas-num').value;
+    test.aktivny=true;
+    let otazky= {};
+    const q=document.getElementsByClassName('question');
+
+    for(let i=0;i<q.length;i++){
+        let q1Id=q[i].id;
+        otazky[i+1]={}
+        if(q[i].classList.contains("type-1")){
+            otazky[i+1].typ=1;
+        }
+        if(q[i].classList.contains("type-2")){
+            otazky[i+1].typ=2;
+        }
+        if(q[i].classList.contains("type-3")){
+            otazky[i+1].typ=3;
+        }
+        otazky[i+1].nazov=q[i].value;
+        const qOpt=document.getElementsByClassName(q1Id+"-option");
+        if(q[i].classList.contains("type-1")===true) {
+
+            let spravne_odpovede = [];
+            for (let j = 0; j < qOpt.length; j++) {
+                spravne_odpovede[j] = qOpt[j].value;
+            }
+
+            otazky[i + 1].spravne_odpovede = spravne_odpovede;
+        }
+        else if(q[i].classList.contains("type-3")===true) {
+            let odpovede=[];
+            for (let j = 0; j < qOpt.length; j++) {
+                let spravnost=false;
+                if(qOpt[j].parentElement.classList.contains('spravna')===true){
+                    spravnost=true;
+                }
+                odpovede[j] = {text: qOpt[j].value , je_spravna: spravnost};
+            }
+            otazky[i + 1].odpovede = odpovede;
+            otazky[i + 1].vie_student_pocet_spravnych=false;
+        }
+    }
+    test.otazky=otazky
+    console.log(JSON.stringify(test));
+}
