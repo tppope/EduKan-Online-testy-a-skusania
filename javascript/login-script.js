839b503267544a1d751c28a4e7b5230f767eef36
Tomáš Popík <74919436+tppope@users.noreply.github.com>
2021-04-30 14:11:11 +0200

    Frontend pre prihlásenie, odhlásenie a registráciu učiteľa hotový.

---
+++ javascript/login-script.js
@@ -0,0 +1,118 @@
+$(window).on("load", function () {
+    checkIfAlreadyLogin();
+    $('[data-toggle="tooltip"]').tooltip();
+    $("#reg-button").hide();
+    checkRegStatus();
+    checkLogoutStatus()
+})
+
+function checkLogoutStatus(){
+    let logoutInfo = sessionStorage.getItem("logoutStatus");
+    if (logoutInfo != null) {
+        if (logoutInfo === 'failed')
+            $("#log-failed-info").text("Najprv sa prosím prihláste");
+        showLogInfo(logoutInfo);
+        sessionStorage.removeItem('logoutStatus');
+    }
+}
+
+function checkRegStatus() {
+    let regInfo = sessionStorage.getItem("regStatus");
+    if (regInfo != null) {
+        showRegInfo(regInfo);
+        sessionStorage.removeItem('regStatus');
+    }
+}
+
+function showRegInfo(regInfo) {
+    let regDiv = $("#reg-" + regInfo);
+    regDiv.css("top", 0);
+    setTimeout(function () {
+        regDiv.css("top", "-100px");
+    }, 3000)
+}
+
+function showLogInfo(regInfo) {
+    let logDiv = $("#log-" + regInfo);
+    logDiv.css("top", 0);
+    setTimeout(function () {
+        logDiv.css("top", "-100px");
+    }, 3000)
+}
+
+function changeLoginType(choiceType) {
+    changeChoice(choiceType);
+    changeContent(choiceType.id);
+}
+
+function changeChoice(choice) {
+    $(".choice-button").css({
+        "background-color": "#f0f0f0",
+        "color": "black",
+    });
+    $(choice).css({
+        "background-color": "white",
+        "color": "black",
+    });
+}
+
+function changeContent(content) {
+    $(".teacher-login-content").hide();
+    $(".student-login-content").hide();
+    $(`.${content}-content`).show();
+}
+
+function submitTeacherLoginForm() {
+    let form = document.getElementById("teacher-login-form");
+    if (checkFormValidation(form)) {
+        let request = new Request('api/uzivatelia/prihlasenie/', {
+            method: 'POST',
+            body: new FormData(form),
+        });
+        fetch(request)
+            .then(response => response.json())
+            .then(data => {
+                let email = $("#email");
+                let password = $("#password");
+                if (!data.error) {
+                    if (data.emailVerify && data.passwordVerify)
+                        window.location.href = 'teacher-homescreen.html';
+                    else if (!data.emailVerify)
+                        email.addClass("is-invalid");
+                    else if (!data.passwordVerify)
+                        password.addClass("is-invalid");
+                } else{
+                    $("#log-failed-info").text("Prihlásenie neprebehlo úspešne. Skúste to prosím znovu.");
+                    showLogInfo(data.status);
+                }
+            });
+    }
+    return false;
+}
+
+function checkFormValidation(form) {
+    let inputs = $(form).find("input");
+    for (let i = 0; i < inputs.length; i++) {
+        if (!inputs.get(i).checkValidity())
+            return false;
+    }
+    return true;
+}
+
+function checkIfAlreadyLogin() {
+    $.getJSON("api/uzivatelia/prihlasenie/", function (data) {
+        if (!data.error) {
+            if (data.alreadyLogin)
+                window.location.href = 'teacher-homescreen.html';
+        }
+    })
+}
+
+function removeEmailIsInvalid() {
+    $("#email").removeClass("is-invalid");
+}
+
+function removePasswordIsInvalid() {
+    $("#password").removeClass("is-invalid");
+}
+
---
+++ javascript/teacher-homescreen-script.js
@@ -0,0 +1,37 @@
+$(window).on("load", function () {
+    $('[data-toggle="tooltip"]').tooltip();
+    getLoggedInUser();
+})
+
+function getLoggedInUser(){
+    $.getJSON("api/uzivatelia/prihlasenie/", function (data) {
+        if (!data.error) {
+            if (data.alreadyLogin) {
+                let userName = data.user.meno + " " + data.user.priezvisko;
+                $("#log-success-info").text("Vitajte "+userName+". Boli ste úspešne prihlásený.");
+                showLogInfo(data.status);
+            }
+            else{
+                sessionStorage.setItem("logoutStatus","failed");
+                window.location.href = 'index.html';
+            }
+        }
+    })
+}
+
+function showLogInfo(regInfo) {
+    let logDiv = $("#log-" + regInfo);
+    logDiv.css("top", 0);
+    setTimeout(function () {
+        logDiv.css("top", "-100px");
+    }, 3000)
+}
+
+function logout(){
+    $.getJSON("api/uzivatelia/odhlasenie/", function (data) {
+        if (!data.error) {
+            sessionStorage.setItem("logoutStatus","success");
+            window.location.href = 'index.html';
+        }
+    })
+}
